6c771474cd38140b42481c6a3df188a8e95bb5e2
luanaparra <luana.silva@sou.inteli.edu.br>
2024-06-19 08:34:45 -0300

    update all ponderadas

---
+++ ponderada4/backend/alembic/env.py
@@ -0,0 +1,34 @@
+from logging.config import fileConfig
+
+from sqlalchemy import engine_from_config
+from sqlalchemy import pool
+
+from src.models import *
+
+from alembic import context
+
+config = context.config
+
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+target_metadata = Base.metadata
+
+def run_migrations_offline() -> None:
+
+    url = config.get_main_option("sqlalchemy.url")
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def run_migrations_online() -> None:
+
+    In this scenario we need to create an Engine
+    and associate a connection with the context.
